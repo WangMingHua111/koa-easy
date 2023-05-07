f6412c2c2731698231d1cf5fa893d3999db575cf
wangminghua <735161977@qq.com>
2023-05-07 14:29:02 +0800

    modified:   package.json 	modified:   vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,11 @@
 import { configDefaults, defineConfig } from 'vitest/config'
-import esbuildPluginTsc from 'esbuild-plugin-tsc'
+// import esbuildPluginTsc from 'esbuild-plugin-tsc'
 export default defineConfig({
-  // esbuild: {
-  //   target: 'es2020',
-  //   plugins: [esbuildPluginTsc()],
+  // optimizeDeps: {
+  //   esbuildOptions: {
+  //     plugins: [esbuildPluginTsc()]
+  //   }
   // },
-  optimizeDeps:{
-    esbuildOptions:{
-      plugins: [esbuildPluginTsc()]
-    }
-  },
   test: {
     include: ['./vitest/**/*.test.ts'],
     exclude: [...configDefaults.exclude],
